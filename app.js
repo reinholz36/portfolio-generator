96fb1a055bf74f63763e62f10627bd710406815c
reinholz36 <dreinholz28@gmail.com>
2022-01-11 11:10:25 -0500

    created files to generate webpage

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-templage');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log("profileDataArgs", profileDataArgs);
 
-//added to bottom
-const printProfileData = profileDataArr => {
-    //This ...
-    for (let i = 0; i < profileDataArr.length; i += 1){
-        console.log('profileDataArr[i]',profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the sames as this...
-    profileDataArr.forEach(profileItem => console.log('profileItem', profileItem));
-};
-
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
 
 
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+}); 
